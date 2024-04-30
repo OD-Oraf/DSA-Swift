e68393bbdbbf72228c67bb1c6f43e4fec5f480d5
odoraf <poseidonog@gmail.com>
2024-04-30 00:09:14 -0400

    Initial Commit

---
+++ DSA-Swift/DSA_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSA_SwiftApp.swift
+//  DSA-Swift
+//
+//  Created by OD Orafidiya on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct DSA_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
